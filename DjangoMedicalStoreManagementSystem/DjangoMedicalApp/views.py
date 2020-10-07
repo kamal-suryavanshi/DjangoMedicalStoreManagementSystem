5b6083ad36a3ccee66dd7947a305e8fce4937c7f
kamal-suryavanshi <33751904+kamal-suryavanshi@users.noreply.github.com>
2020-10-07 23:58:49 +0530

    Initial commit

---
+++ DjangoMedicalStoreManagementSystem/DjangoMedicalApp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+
+# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from DjangoMedicalApp.models import Company
+
+from DjangoMedicalApp.serializer import CompanySerializer
+
+
+class CompanyViewSet(viewsets.ViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        company = Company.objects.all()
+        serializer = CompanySerializer(company, many=True, context={"request": request})
+        dict_response = {"error": False, "message": "All Company List Data", "data": serializer.data}
+        return Response(dict_response)
+
+    def create(self, request):
+        try:
+            serializer = CompanySerializer(data=request.data, context={"request": request})
+            serializer.is_valid()
+            serializer.save()
+            dict_response = {"error": False, "message": "Company Data Saved Successfully"}
+        except:
+            dict_response = {"error": False, "message": "Error During Saving Company Data"}
+        return Response(dict_response)
+
+    def update(self, request, pk=None):
+        try:
+            queryset = Company.objects.all()
+            company = get_object_or_404(queryset, pk=pk)
+            serializer = CompanySerializer(company, data=request.data, context={"request": request})
+            serializer.is_valid()
+            serializer.save()
+            dict_response = {"error": False, "message": "Company Data Updated Successfully"}
+        except:
+            dict_response = {"error": False, "message": "Error During Updating Company Data"}
+        return Response(dict_response)
+
+
+company_list = CompanyViewSet.as_view(actions={"get": "list"})
+company_create = CompanyViewSet.as_view(actions={"post": "create"})
+company_update = CompanyViewSet.as_view(actions={"put": "update"})
